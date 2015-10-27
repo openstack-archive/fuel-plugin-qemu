63fb920a81bf62187c6c995ef7a52ddd877f33ba
OpenStack Project Creator <openstack-infra@lists.openstack.org>
2015-10-27 12:52:54 +0000

    Implement: Fuel install QEMU
    Todo lists:
    1. update dependencies.txt after RPM/debian packages are uploaded to official site
    
    Change-Id: I05515b20f1e368cebc53a29762ca646af88591ac
    Closes-Bug:#1507878
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

---
+++ deployment_scripts/puppet/manifests/qemu-install.pp
@@ -0,0 +1,24 @@
+$qemu_version = "1:2.4+dfsg-4ubuntu1"
+if $operatingsystem == 'Ubuntu' {
+        package { 'qemu-block-extra':
+                ensure => "${qemu_version}",
+        }
+        package { 'qemu-utils':
+                ensure => "${qemu_version}",
+                require => Package['qemu-block-extra'],
+        }
+        package { 'qemu-system':
+                ensure => "${qemu_version}",
+        }
+        package { 'qemu-user':
+                ensure => "${qemu_version}",
+        }
+	package { 'qemu':
+		ensure => "${qemu_version}",
+                require => Package['qemu-utils','qemu-system','qemu-user'],
+	}
+} elsif $operatingsystem == 'CentOS' {
+	package { 'qemu':
+		ensure => "2.4.1-1",
+	}
+}
