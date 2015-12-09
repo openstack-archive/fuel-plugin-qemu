683d115b0e2dccc73a9d411b55e9584e95b1eebb
lingyu1 <ling.y.yu@intel.com>
2015-12-10 01:50:36 +0800

    Update KVM
    Closes-Bug:#1507878
    Change-Id: Ib6cd914df0b7a2f5f5069a1abf09eaf90e6b8559
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

--- deployment_scripts/puppet/manifests/qemu-install.pp
+++ deployment_scripts/puppet/manifests/qemu-install.pp
@@ -1,40 +1,50 @@
+$fuel_settings = parseyaml(file('/etc/compute.yaml'))
 $qemu_version = "1:2.4+dfsg-4ubuntu1"
 if $operatingsystem == 'Ubuntu' {
-        package { 'qemu-block-extra':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-utils':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-common':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-arm':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-mips':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-misc':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-sparc':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-x86':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system-ppc':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-user':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu-system':
-                ensure => "${qemu_version}",
-        }
-        package { 'qemu':
-                ensure => "${qemu_version}",
+        if $fuel_settings['fuel-plugin-qemu']['use_kvm'] {
+                package { 'linux-headers-4.1.10-rt10nfv':
+                        ensure => "1.0.OPNFV",
+                }
+                package { 'linux-image-4.1.10-rt10nfv':
+                        ensure => "1.0.OPNFV",
+                }
+        } else {
+                package { 'qemu-block-extra':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-utils':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-common':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-arm':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-mips':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-misc':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-sparc':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-x86':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system-ppc':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-user':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu-system':
+                        ensure => "${qemu_version}",
+                }
+                package { 'qemu':
+                        ensure => "${qemu_version}",
+                }
         }
 } elsif $operatingsystem == 'CentOS' {
 }
