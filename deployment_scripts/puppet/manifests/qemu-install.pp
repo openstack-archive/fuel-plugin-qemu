a394c5666471f68409c53dfb9ccdd75411e3428e
lingyu1 <ling.y.yu@intel.com>
2015-12-18 21:06:02 +0800

    Implement: Fuel install QEMU
    Closes-Bug:#1507878
    
    Change-Id: I025e1c9a8091451c74329f2e569265ce06ab69d3
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

--- deployment_scripts/puppet/manifests/qemu-install.pp
+++ deployment_scripts/puppet/manifests/qemu-install.pp
@@ -4,9 +4,13 @@ if $operatingsystem == 'Ubuntu' {
         if $fuel_settings['fuel-plugin-qemu']['use_kvm'] {
                 package { 'linux-headers-4.1.10-rt10nfv':
                         ensure => "1.0.OPNFV",
-                }
+                } ->
                 package { 'linux-image-4.1.10-rt10nfv':
                         ensure => "1.0.OPNFV",
+                } ->
+                exec {'reboot':
+                command => "reboot",
+                path   => "/usr/bin:/usr/sbin:/bin:/sbin",
                 }
         } else {
                 package { 'qemu-block-extra':
