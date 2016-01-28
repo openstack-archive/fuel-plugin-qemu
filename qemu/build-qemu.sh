8b2337250827e3170005868def69340790196f2f
lingyu1 <ling.y.yu@intel.com>
2016-01-29 05:34:59 +0800

    Fix qemu/kvm bug
    Change-Id: I09267250f901c1191c6b483b0d46f2279f73de61
    Signed-off-by: lingyu1 <ling.y.yu@intel.com>

--- qemu/build-qemu.sh
+++ qemu/build-qemu.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
-wget http://kr.archive.ubuntu.com/ubuntu/pool/main/libt/libtool/libtool_2.4.2-1.11_all.deb
-wget http://kr.archive.ubuntu.com/ubuntu/pool/main/libt/libtool/libtool-bin_2.4.2-1.11_amd64.deb
-sudo dpkg -i libtool_2.4.2-1.11_all.deb
-sudo dpkg -i libtool-bin_2.4.2-1.11_amd64.deb
-dpkg-source -x qemu_2.4+dfsg-4ubuntu1.dsc
-cd /qemu-2.4+dfsg;sudo dpkg-buildpackage
+wget http://wiki.qemu-project.org/download/qemu-2.2.1.tar.bz2
+sudo apt-get build-dep qemu -y
+sudo apt-get install devscripts -y
+sudo apt-get install dpkg-dev -y
+apt-get source qemu -y
+dpkg-source -x qemu_2.0.0+dfsg-2ubuntu1.21.dsc
+cd qemu-2.0.0+dfsg; uupdate -v 2.2.1 ../qemu-2.2.1.tar.bz2
+cd ../qemu-2.2.1;echo "">> debian/patches/series
+sed -i 's/seccomp="yes"/seccomp="no"/' configure
+debian/rules build
+fakeroot debian/rules binary
--- qemu/download-qemu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://launchpad.net/ubuntu/+archive/primary/+files/qemu_2.4%2Bdfsg.orig.tar.xz
-wget https://launchpad.net/ubuntu/+archive/primary/+files/qemu_2.4%2Bdfsg-4ubuntu1.debian.tar.xz
-wget https://launchpad.net/ubuntu/+archive/primary/+files/qemu_2.4%2Bdfsg-4ubuntu1.dsc
